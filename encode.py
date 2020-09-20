cff0afc2da0f3cf8001cd79e14555bd7bd8c7ad6
calvin-brown <44214170+calvin-brown@users.noreply.github.com>
2020-09-19 21:03:26 -0700

    added plotting of encoded spectra

--- encode.py
+++ encode.py
@@ -11,6 +11,7 @@ from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras import optimizers
 from scipy.signal import find_peaks
+from matplotlib import pyplot as plt
 
 
 # load spectral data
@@ -22,14 +23,16 @@ test_frac = 0.1
 split_idx = np.round(n_spectra * (1 - test_frac))
 X_train = spectra[:split_idx, :]
 X_test = spectra[split_idx:, :]
+n_train = X_train.shape[0]
+n_test = X_test.shape[0]
 
 # create autoencoder model
-input_layer = Input(shape=(n_wavelengths,))
-encoded = Dense(32, activation='relu')(input_layer)
+input_spectrum = Input(shape=(n_wavelengths,))
+encoded = Dense(32, activation='relu')(input_spectrum)
 decoded = Dense(n_wavelengths, activation='sigmoid')(encoded)
-autoencoder = Model(input_layer, decoded)
+autoencoder = Model(input_spectrum, decoded)
 
-encoder = Model(input_layer, encoded)
+encoder = Model(input_spectrum, encoded)
 
 encoded_input = Input(shape=(32,))
 decoder_layer = autoencoder.layers[-1]
@@ -41,3 +44,27 @@ autoencoder.compile(optimizer=opt, loss='binary_crossentropy')
 # train model
 autoencoder.fit(X_train, X_train, epochs=50, batch_size=256,
                 validation_data=(X_test, X_test))
+
+# evaluate model
+encoded_spectra = encoder.predict(X_test)
+decoded_spectra = decoder.predict(encoded_spectra)
+
+for i in np.random.choice()
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
