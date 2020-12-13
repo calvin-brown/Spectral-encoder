e850dafffcf78b7a4f97b9a8b56f8adc27f43994
calvin-brown <44214170+calvin-brown@users.noreply.github.com>
2020-12-12 18:36:29 -0800

    Add files via upload

--- generate_spectra.py
+++ generate_spectra.py
@@ -5,6 +5,8 @@ Created on Wed Dec  9 09:13:24 2020
 @author: Calvin Brown
 """
 
+import pickle
+
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import norm
@@ -17,6 +19,7 @@ def calculate_std(fwhm):
 def create_random_spec():
     curr_spectrum = np.zeros(n_wavelengths)
     n_peaks = np.random.randint(1, max_n_peaks + 1)
+    peak_data = [[], [], []]
 
     for peak in range(n_peaks):
         center = np.random.choice(wavelengths)
@@ -26,17 +29,21 @@ def create_random_spec():
         curr_peak = norm.pdf(wavelengths, center, calculate_std(fwhm))
         curr_peak = curr_peak * (power/np.max(curr_peak))
         curr_spectrum += curr_peak
+        peak_data[0].append(center)
+        peak_data[1].append(power)
+        peak_data[2].append(fwhm)
 
     if add_noise:
         curr_spectrum += np.random.normal(0, noise_power, n_wavelengths)
 
-    return np.abs(curr_spectrum)
+    # return np.abs(curr_spectrum), peak_data
+    return curr_spectrum, peak_data
 
 
 # Define constants.
 is_narrowband = False
-add_noise = False
-n_spectra = 100000
+add_noise = True
+n_spectra = 1000000
 min_wavelength = 400
 max_wavelength = 700
 n_wavelengths = 1000
@@ -45,21 +52,25 @@ max_n_peaks = 4
 min_power = 0.1
 max_power = 1
 power_range = max_power - min_power
-noise_power = 0.001
+noise_power = 0.003
 if is_narrowband:
     min_fwhm = 5
     max_fwhm = 15
 else:
-    min_fwhm = 10
-    max_fwhm = 30
+    min_fwhm = 2  # 10
+    max_fwhm = 100  # 30
 fwhm_range = max_fwhm - min_fwhm
 
 # Generate random spectra.
 spectra = np.zeros((n_spectra, n_wavelengths), dtype='float32')
+peaks = []
 for i in range(n_spectra):
-    spectra[i, :] = create_random_spec()
+    spectra[i, :], curr_peak_data = create_random_spec()
+    peaks.append(curr_peak_data)
 
 np.save(f'spectra{"_narrow" if is_narrowband else ""}', spectra)
+with open('peak_data.txt', 'wb') as f:
+    pickle.dump(peaks, f)
 
 # Plot random spectra.
 fig, ax = plt.subplots(figsize=(9, 5))
